d737cd79c199de848607150c56025099057ddb42
Sean Shubin <sshubin@cj.com>
2016-05-09 18:37:55 -0700

    Fixed dispatcher

--- core/src/main/scala/com/seanshubin/todo/persistence/core/Dispatcher.scala
+++ core/src/main/scala/com/seanshubin/todo/persistence/core/Dispatcher.scala
@@ -3,10 +3,10 @@ package com.seanshubin.todo.persistence.core
 class Dispatcher(interpreter: Interpreter) extends RequestValueHandler {
   override def handle(request: RequestValue): ResponseValue = {
     (request.method, request.subject) match {
-      case ("POST", "task-event") =>
+      case ("POST", "/task-event") =>
         val responseBody = interpreter.execute(request.body)
         ResponseValue(201, responseBody)
-      case ("GET", "task") =>
+      case ("GET", "/task") =>
         val responseBody = TaskFormatter.tasksToString(interpreter.tasks)
         ResponseValue(200, responseBody)
       case _ => ResponseValue(405, s"Not allowed to apply method ${request.method} to subject ${request.subject}")
