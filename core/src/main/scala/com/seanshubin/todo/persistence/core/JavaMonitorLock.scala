a5e6ae0f7af1e2dfcfaca429bea306e533079e53
Sean Shubin <sshubin@cj.com>
2016-05-31 14:21:58 -0700

    Formatting

--- core/src/main/scala/com/seanshubin/todo/persistence/core/JavaMonitorLock.scala
+++ core/src/main/scala/com/seanshubin/todo/persistence/core/JavaMonitorLock.scala
@@ -1,7 +1,9 @@
 package com.seanshubin.todo.persistence.core
 
 class JavaMonitorLock(monitor: AnyRef) extends Lock {
-  override def doWithLock[T](f: => T): T = monitor.synchronized {
-    f
+  override def doWithLock[T](f: => T): T = {
+    monitor.synchronized {
+      f
+    }
   }
 }
