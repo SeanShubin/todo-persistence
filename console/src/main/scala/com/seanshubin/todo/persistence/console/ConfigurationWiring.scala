269c7b321d4eff3d480a37f34722a36c33460359
Sean Shubin <sshubin@cj.com>
2016-06-23 16:08:54 -0700

    Clarifications in wiring

--- console/src/main/scala/com/seanshubin/todo/persistence/console/ConfigurationWiring.scala
+++ console/src/main/scala/com/seanshubin/todo/persistence/console/ConfigurationWiring.scala
@@ -29,33 +29,34 @@ import org.eclipse.jetty.server.Handler
 trait ConfigurationWiring {
   def configuration: Configuration
 
-  lazy val port: Int = configuration.port
-  lazy val dataFileDirectory: Path = configuration.dataFileDirectory
-  lazy val monitor = new AnyRef
-  lazy val dataFileName: String = "tasks.txt"
-  lazy val healthCheckFileName: String = "health.txt"
-  lazy val charset: Charset = StandardCharsets.UTF_8
   lazy val initialTasks: Tasks = Tasks.Empty
+  lazy val monitor = new AnyRef
+  lazy val clock: Clock = Clock.systemUTC()
   lazy val statefulInterpreter: Interpreter =
     new StatefulInterpreterNotThreadSafe(initialTasks)
-  lazy val clock: Clock = Clock.systemUTC()
-  lazy val files: FilesContract = FilesDelegate
   lazy val lock: Lock = new JavaMonitorLock(monitor)
+  lazy val dataFileName: String = "tasks.txt"
+  lazy val files: FilesContract = FilesDelegate
+  lazy val dataFileDirectory: Path = configuration.dataFileDirectory
+  lazy val healthCheckFileName: String = "health.txt"
+  lazy val charset: Charset = StandardCharsets.UTF_8
   lazy val storingInterpreter: Interpreter = new StoringInterpreter(
     clock, files, statefulInterpreter, dataFileDirectory, lock, dataFileName, charset)
-  lazy val loadingInterpreter: Interpreter = new LoadingInterpreter(statefulInterpreter)
   lazy val healthCheckHandler: HealthCheckHandlerMarker = new HealthCheckHandler(
     files, dataFileDirectory, healthCheckFileName, charset)
   lazy val taskHandler: TaskHandlerMarker = new TaskHandler(statefulInterpreter)
   lazy val taskEventHandler: TaskEventHandlerMarker = new TaskEventHandler(storingInterpreter)
-  lazy val dispatcher: RequestValueHandler =
-    new DispatchPaths(healthCheckHandler, taskHandler, taskEventHandler)
+  lazy val dispatcher: RequestValueHandler = new DispatchPaths(healthCheckHandler, taskHandler, taskEventHandler)
+  lazy val loadingInterpreter: Interpreter = new LoadingInterpreter(statefulInterpreter)
+  lazy val port: Int = configuration.port
+  lazy val createJettyServer: Int => JettyServerContract = JettyServerDelegate.create
   lazy val handlerAdapter: Handler = new HandlerAdapter(dispatcher, charset)
   lazy val preLoader: PreLoader = new FileSystemPreLoader(
     dataFileDirectory, files, charset, loadingInterpreter, dataFileName)
   lazy val runner: Runnable = new JettyRunner(
-    port, JettyServerDelegate.create, handlerAdapter, preLoader)
+    port, createJettyServer, handlerAdapter, preLoader)
 }
+
 /*
 Alternative designs to consider:
 
